4c1cc680e172d409823c64378c20c8aacc6c0ace
ramseysisko <106939319+ramseysisko@users.noreply.github.com>
2022-07-12 02:36:28 +0200

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,15 @@
-cbcbcvb
+#include "main.h"
+/**
+ * swap_int - swaps integers using pointers
+ * @a: integer pointer a
+ * @b: integer pointer b
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
